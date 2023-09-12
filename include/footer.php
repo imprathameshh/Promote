0779a9616794455d624c75fe8f705f81ee1da894
prathmesh4 <prathmesh@krishaweb.com>
2023-09-12 14:22:28 +0530

    Final

---
+++ include/footer.php
@@ -0,0 +1,110 @@
+	<!-- FooterSec Start -->
+	<div class="footer-wrap">
+		<div class="container">
+			<div class="footer-content">
+				<div class="footer-content-left">
+
+					<div class="footer-card">
+						<div class="footer-logo">
+							<img src="assets/images/footer-logo.png" alt="">
+						</div>
+						<div class="img-wrap">
+							<img src="assets/images/footer-card-img.png" alt="">
+						</div>
+						<div class="footer-card-content">
+							<h5>Subscribe now</h5>
+							<p>Industry's standard from dummy and make a type book. </p>
+						</div>
+						<div class="card-btn">
+							<input type="text" name="name" value="" placeholder="Enter Your Email">
+							<a href="#" class="btn-yellow">Subscribe</a>
+						</div>
+					</div>
+				</div>
+				<div class="footer-content-right">
+					<div class="footer-right-head row">
+						<div class="footer-head-left col-md-6 col-sm-12">
+							<div class="get-touch">
+								<img src="assets/images/icons/footer-call.png" alt="">
+								<h5>Get in touch with</h5>
+							</div>
+							<div class="more-contect">
+								<a href="tel:+1 917 000 1212">+1 917 000 1212</a>
+								<a href="mailto:nextlevel@marketing.com"> nextlevel@marketing.com</a>
+							</div>
+					
+						</div>
+						<div class="footer-head-right col-md-6 col-sm-12">
+							<div class="location">
+								<img src="assets/images/icons/location.png" alt="">
+								<h5>Location</h5>
+							</div>
+							<p>10 Suna House 65 Rivington Street London EC2A 3QQ</p>
+							<a href="#">Location</a>
+						</div>
+					</div>
+
+					<div class="footer-list-wrap row">
+
+						<div class="footer-list col-4">
+							<div class="footer-list-title">
+								<img src="assets/images/icons/footer-list-head-1.png" alt="icon" class="icon">
+								<div class="title">Pages</div>
+							</div>
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="aboutus.html">About</a></li>
+								<li><a href="blog-single.html">Blog</a></li>
+								<li><a href="#">Pricing Single</a></li>
+								<li><a href="contact-us.html">Contact</a></li>
+								<li><a href="services-single.html">Services</a></li>
+							</ul>
+						</div>
+
+						<div class="footer-list col-4">
+							<div class="footer-list-title">
+								<img src="assets/images/icons/footer-list-head-2.png" alt="icon" class="icon">
+								<div class="title">Pages</div>
+							</div>
+							<ul>
+								<li><a href="">Styleguide</a></li>
+								<li><a href="not-found.html"> 404 Not Found</a></li>
+								<li><a href="password.html"> Password Protected</a></li>
+								<li><a href="license.html"> Licenses</a></li>
+								<li><a href="changelogo.html"> Changelog</a></li>
+							</ul>
+						</div>
+
+
+						<div class="footer-list col-4">
+							<div class="footer-list-title">
+								<img src="assets/images/icons/footer-list-head-3.png" alt="icon" class="icon">
+								<div class="title">Pages</div>
+							</div>
+							<ul>
+								<li><a href="#">Facebook</a></li>
+								<li><a href="#"> Twitter</a></li>
+								<li><a href="#"> Instagram</a></li>
+								<li><a href="#"> LinkedIn</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="footer-copyright">
+						<p>Copyright © Nextlevel | Designed by VictorFlow - Powered by Webflow</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- FooterSec End -->
+	
+	<!-- JS File -->
+	<script src="assets/js/jquery.min.js"></script>
+	<!-- <script src="assets/js/jquery.validate.min.js"></script> -->
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/slick.min.js"></script>
+	<!-- Custom JS File -->
+	<script src="assets/js/setting.js"></script>
---
+++ include/head.php
@@ -0,0 +1,27 @@
+<!-- Basic Page Info -->
+<meta charset="UTF-8">
+
+<!-- Theme Color -->
+<meta name="theme-color" content="#FFF">
+
+<!-- Title -->
+<title>Promote</title>
+
+<!-- Site favicon -->
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!-- Mobile Specific Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- Google Font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900&family=Nunito:wght@300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
+
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+<link rel="stylesheet" type="text/css" href="assets/css/jquery.fancybox.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/css/media.css">
